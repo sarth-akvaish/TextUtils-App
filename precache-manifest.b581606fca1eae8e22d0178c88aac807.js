6139a828e39603f086b85a9bc7dbcde575f8316a
sarth-akvaish <sarthakvkumar@gmail.com>
2022-09-03 11:27:55 +0530

    Updates

---
+++ precache-manifest.b581606fca1eae8e22d0178c88aac807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4352f4ed026137258bbf",
+    "url": "/TextUtils-App/static/css/main.2bd5beab.chunk.css"
+  },
+  {
+    "revision": "4352f4ed026137258bbf",
+    "url": "/TextUtils-App/static/js/main.4352f4ed.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/TextUtils-App/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-App/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "0f6e48f37ede4d777cc7",
+    "url": "/TextUtils-App/static/js/runtime~main.0f6e48f3.js"
+  },
+  {
+    "revision": "8c93002d6f2d84944f1938d4ec2c6e16",
+    "url": "/TextUtils-App/index.html"
+  }
+];
